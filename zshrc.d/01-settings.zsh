55e84736bc03a77ae8ea2b2ef44209985a513c70
Hans Gaiser <hansg91@gmail.com>
2022-01-01 22:54:10 +0100

    Fix familiar keybindings.

--- zshrc.d/01-settings.zsh
+++ zshrc.d/01-settings.zsh
@@ -4,9 +4,9 @@ HISTFILE=$ZDOTDIR/history
 HISTSIZE=100000
 SAVEHIST=100000
 
-setopt auto_cd # If the command is a directory name, cd into it.
-setopt appendhistory # Append to history, instead of replacing it.
-setopt auto_pushd # Make cd push the old directory onto the directory stack.
+setopt auto_cd          # If the command is a directory name, cd into it.
+setopt appendhistory    # Append to history, instead of replacing it.
+setopt auto_pushd       # Make cd push the old directory onto the directory stack.
 setopt hist_ignore_dups # Ignore duplicates for history.
-setopt menu_complete # On completion, insert the first match right away.
+setopt menu_complete    # On completion, insert the first match right away.
 setopt null_glob
