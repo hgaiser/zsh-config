baa2c00f9bfb9d145bd9f7010116f338c7f8cbf6
Hans Gaiser <hansg91@gmail.com>
2022-01-12 15:02:14 +0100

    Add mkd utility.

--- zshrc.d/02-utils.zsh
+++ zshrc.d/02-utils.zsh
@@ -9,3 +9,17 @@ function test_command {
 function try_source {
 	[ -f "$1" ] && source "$1"
 }
+
+# Create a directory and change to it.
+function mkd {
+	if [ $# -gt 1 ]; then
+		echo "Usage: $0 [dir]" >&2
+		return 13
+	fi
+	if [ -z $1 ]; then
+		mkd "/tmp/$(date +%F-%H-%M-%S)"
+	else
+		mkdir -p "$1"
+		cd "$1"
+	fi
+}
