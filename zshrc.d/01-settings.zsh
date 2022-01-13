b889027c6415e6bf013bebce7061b47e98eae9ac
Hans Gaiser <hansg91@gmail.com>
2022-01-13 15:27:08 +0100

    Change character to > symbol.

--- zshrc.d/01-settings.zsh
+++ zshrc.d/01-settings.zsh
@@ -13,5 +13,4 @@ WORDCHARS=${WORDCHARS/\/}
 setopt appendhistory    # Append to history, instead of replacing it.
 setopt auto_pushd       # Make cd push the old directory onto the directory stack.
 setopt hist_ignore_dups # Ignore duplicates for history.
-setopt menu_complete    # On completion, insert the first match right away.
 setopt null_glob
